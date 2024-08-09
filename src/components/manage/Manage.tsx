c9df571744e01523bc0fcb5500ad406ecc3fa4ce
nik0504030201 <nikita0504030201@gmail.com>
2024-08-09 17:44:00 +0500

    alpha

--- src/components/manage/Manage.tsx
+++ src/components/manage/Manage.tsx
@@ -6,34 +6,37 @@ import {useRouter} from "next/navigation";
 import Marquee from "@/components/Marquee/Marquee";
 import useStore from "@/Store";
 
-const Manage = () => {
+const ContinueManage = () => {
+    const zoom= useStore((state:any)=> state.zoom);
     const content= useStore((state:any)=> state.Content);
-    const [show, setShow] = React.useState(true);
+    const [show, setShow] = React.useState<boolean>(true);
     const router = useRouter();
-    const [delay, setDelay] = React.useState(1);
+    const [delay, setDelay] = React.useState<number>(1);
     setTimeout(() => {setDelay(0)}, 1000)
+    const toggleZoom= useStore((state:any)=> state.toggleZoom);
     return (
         <>
-            <Marquee side={`left`} show={show} delay={delay} content={content}/>
-            <Marquee side={`right`} show={show} delay={delay} content={content}/>
+            <Marquee show={show} delay={delay} content={content}/>
             <AnimatePresence>
                 {show && (
                     <motion.div
-                        initial={{transform: `translateY(-150%) rotate(180deg)`, opacity: 0}}
-                        animate={{transform: `rotate(180deg)`, opacity: 1}}
-                        exit={{transform: `translateY(-150%) rotate(180deg)`, opacity: 0}}
-                        transition={{duration: 1, delay: delay}}>
+                        initial={{transform: `translateY(-150%)`, opacity: 0}}
+                        animate={{transform: `translateY(0)`, opacity: 1}}
+                        exit={{transform: `translateY(-150%)`, opacity: 0}}
+                        transition={{duration: 1, delay: delay}}
+                        className="absolute top-[25px] left-[25px]">
                         <Image
-                            src="/arrow_down.svg"
+                            src="/arrowleft.svg"
                             loading={`lazy`}
-                            width={`350`}
-                            height={`350`} alt={""}
+                            width={`95`}
+                            height={`45`} alt={""}
                             className={`hoverEffectArrow duration-500 pb-[20px]`}
                             onClick={() => {
+                                setShow(false)
                                 setTimeout(() => {
                                     router.push("/#catalog")
                                 }, 1000)
-                                setShow(false)
+                                toggleZoom(false)
                             }}
                         />
                     </motion.div>
@@ -43,11 +46,11 @@ const Manage = () => {
                 {show && (
                     <motion.aside
                         initial={{transform: `translateX(-50%) translateY(60vw)`, opacity: 0}}
-                        animate={{transform: `translateX(-50%) translateY(-50%)`, opacity: 1}}
+                        animate={{transform: `translateX(-50%) translateY(-50%) ${zoom ? `scale(0.5)` : `scale(1)`}`, opacity: 1}}
                         exit={{transform: `translateX(-50%) translateY(60vw)`, opacity: 0}}
                         transition={{duration: 1}}
-                        className={`absolute -translate-x-1/2 -translate-y-1/2 top-[50%] left-[50%] flex flex-col items-center justify-between gap-[40px] p-[2%] w-[600px] aspect-[1.2] bg-[#000000] bg-cover hover:z-[50] duration-100 `}>
-                        <h2 className={`font-semibold text-[40px] text-[#ffffff]`}>Ведение</h2>
+                        className={`absolute -translate-x-1/2 -translate-y-1/2 top-[${zoom ? `50%` : `50%`}] ${zoom ? `left-[300px]` : `left-[50%]`} flex flex-col items-center justify-between gap-[40px] p-[2%] w-[600px] aspect-[1.2] bg-[#888888] bg-cover hover:z-[50] duration-500 `}>
+                        <h2 className={`font-semibold text-[40px] text-[#ffffff]`}>Ведение существующего</h2>
                         <p className={`font-medium text-[22px] w-[100%] text-start text-[#ffffff]`}>Lorem ipsum dolor
                             sit
                             amet, consectetur adipisicing elit. Fugiat fugit libero quam rem sit, velit vitae. Ab
@@ -69,4 +72,4 @@ const Manage = () => {
     );
 };
 
-export default Manage;
+export default ContinueManage;
